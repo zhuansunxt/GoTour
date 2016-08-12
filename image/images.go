873f08a27a82d1c4d443535922230527775e74be
Xiaotong Sun <sancruise123@gmail.com>
2016-08-11 17:00:19 -0700

    Add solution to image practice

---
+++ image/images.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct {
+	width, height int
+}
+
+// Implement necessary methods of Image interface
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel;
+}
+
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, img.width, img.height)
+}
+
+func (img Image) At(x, y int) color.Color {
+
+	image_generator := func (x, y int) uint8 {
+		return uint8((x+y)/2)
+	} 
+
+	v := image_generator(x, y)
+
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	w, h := 128, 128
+	m := Image{}
+	pic.ShowImage(m)
+}
